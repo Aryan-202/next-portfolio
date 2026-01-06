c46cc9670059bef2e6e7d5b008be3fa110d39fff
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-01-06 21:39:22 +0530

    barrel export through index.ts

--- data/personal-info.ts
+++ data/personal-info.ts
@@ -2,5 +2,5 @@ export const PROFILE_INFO = {
     name: 'Aryan Vishwakarma',
     email: 'aryanvishwakarma275@gmail.com',
     profileImage: '/image/profile/logo.jpg',
-    mobile: 9455289155
+    mobile: '+91 9455289155'
 } as const
