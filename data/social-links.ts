dc20663567947abc6298c2705006958b1fa4514c
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-01-08 22:00:46 +0530

    prittier

--- data/social-links.ts
+++ data/social-links.ts
@@ -1,23 +1,23 @@
 export interface SocialLinks {
-    name :string,
-    href: string,
-    icon: string
+  name: string;
+  href: string;
+  icon: string;
 }
 
 export const SOCIAL_LINKS: SocialLinks[] = [
-    {
-        name: 'GitHub',
-        href: 'https://github.com/Aryan-202',
-        icon: 'github'
-    },
-    {
-        name: 'LinkedIn',
-        href: 'https://www.linkedin.com/in/aryanspf/',
-        icon: 'linkedin'
-    },
-    {
-        name: 'X',
-        href: 'https://x.com/AryanxSenpai',
-        icon: 'x'
-    },
-] as const
+  {
+    name: "GitHub",
+    href: "https://github.com/Aryan-202",
+    icon: "github",
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/aryanspf",
+    icon: "linkedin",
+  },
+  {
+    name: "X",
+    href: "https://x.com/AryanxSenpai",
+    icon: "x",
+  },
+] as const;
