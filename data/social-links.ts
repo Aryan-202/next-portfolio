ea965d853ffd0d5190dd5867996e3452d508d4ba
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-01-09 10:46:33 +0530

    declaration files

--- data/social-links.ts
+++ data/social-links.ts
@@ -1,23 +1,26 @@
+import { assets } from "@/public/assets";
+import React from "react";
+
 export interface SocialLinks {
   name: string;
   href: string;
-  icon: string;
+  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
 }
 
 export const SOCIAL_LINKS: SocialLinks[] = [
   {
     name: "GitHub",
     href: "https://github.com/Aryan-202",
-    icon: "github",
+    icon: assets.logos.github,
   },
   {
     name: "LinkedIn",
     href: "https://www.linkedin.com/in/aryanspf",
-    icon: "linkedin",
+    icon: assets.logos.linkedin,
   },
   {
     name: "X",
     href: "https://x.com/AryanxSenpai",
-    icon: "x",
+    icon: assets.logos.x,
   },
 ] as const;
